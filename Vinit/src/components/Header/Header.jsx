4c7826e3a6768bc030161eb23ad37bca1f2859d7
Vinit Kumar <vinit71811@gmail.com>
2024-11-15 00:33:53 +0530

    Day 2 Tasks are done

--- Vinit/src/components/Header/Header.jsx
+++ Vinit/src/components/Header/Header.jsx
@@ -1,37 +1,22 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+
 import "./Header.css"
 const Header = () => {
   return (
-    
-
-      <Header >
-
-      <div>
-        className="h-screen bg-cover bg-center relative"
-        style={{
-          backgroundImage: "url('/public/images/image 1.png')",
-        }}
-      
-        
-        <div className="absolute inset-0 bg-black bg-opacity-40"></div>
-
-        <div className="relative container mx-auto flex items-center justify-center h-full px-4">
-          <div className="text-center text-white">
-            
-            <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-              Navigate Your Academic Journey with Confidence
-            </h1>
-      
-            <h2 className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">
-              Providing high-quality and comprehensive assistance with assignments
-              and thesis writing for students at all academic levels
-            </h2>
-          </div>
-        </div>
-      </div>
-      </Header>
-    
-
+  
+  <div className="hero-container">
+    <div className="header-content-img">
+      <p id="m-p-1">Navigate Your </p>
+      <p id="m-p-2">Academic Journey</p>
+      <p id="m-p-3">with Confidence</p>
+      <p>
+        Providing high-quality and comprehensive assistance with assignments
+        and thesis writing for students at all academic levels.
+      </p>
+    </div>
+  </div>
+  
   )
 }
 
